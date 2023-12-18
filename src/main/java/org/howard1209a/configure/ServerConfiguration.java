c403739db0c3d52ee41fb2c06a5d54728d962689
howard1209a <howard1209a@163.com>
2023-12-18 08:47:40 +0800

    v1.0

---
+++ src/main/java/org/howard1209a/configure/ServerConfiguration.java
@@ -0,0 +1,9 @@
+package org.howard1209a.configure;
+
+public class ServerConfiguration {
+    private static final ServerConfiguration conf = new ServerConfiguration();
+
+    private ServerConfiguration() {
+
+    }
+}
